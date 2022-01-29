8a03b83ebcfdba25324a1321ef2e5cab84dc5c34
a91n0d <a91n0d@gmail.com>
2022-01-29 11:43:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Algorithmization_and_programming_HomeWork01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x = (6 * 6 - 1) / 2.0f;
+            Console.WriteLine($"Результат вычисления: {x}");
+        }
+    }
+}
